809d5994439bddd96d760db924ca6428ed0e5671
yfxiang <964299178@qq.com>
2018-02-03 16:19:21 +0800

    "调整读取配置文件"

--- module/config.py
+++ module/config.py
@@ -1,51 +1,89 @@
 # -*- coding:utf-8 -*-
 
 __author__ = 'yfx'
-import logging.config
 import configparser
+from .log import MyLogger
 
 
-class RunConfig():
+class ConfigInfo():
     
     def __init__(self, conf_file):
-        config = configparser.ConfigParser()
-        config.read(conf_file, encoding='utf-8')
-
+        self.config = configparser.ConfigParser()
+        self.config.read(conf_file, encoding='utf-8')
+        
+    def get_mode(self):
+        """
+        返回测试接口的列表[图像集合对象，视频片段集合对象]
+        """
+        mode = []
         try:
-            self.run_mode = config['RUNMODE']['runmode']
-            self.person_num = config['SUB']['person_num']
-            self.face_num = config['SUB']['face_num']
-            self.motorvehicle_num = config['SUB']['motorvehicle_num']
-            self.data_type = config['SUB']['data_type']
-            self.sub_num = config['SUB']['sub_num']
-            self.host = config['HOST']['host']
-            self.report_file = config['REPORT']['report_file']
-        except Exception as e:
-            logging.config.fileConfig('../config/logger.conf')
-            logger = logging.getLogger('test')
-            logger.error('读取配置文件失败: %s' % e)
+            image = self.config.get('RUNMODE','image')
+            videoslice = self.config.get('RUNMODE','videoslice')
+        except Exception as error:
+            logger = MyLogger()
+            logger.error('测试运行模式读取失败：%s' % error)
             exit()
+        mode.append(int(image))
+        mode.append(int(videoslice))
+        return mode
 
-    def get_run_mode(self):
-        return self.run_mode
+    def get_image(self):
+        """
+        返回图片配置信息的列表[图片个数，是否包含Data信息]
+        """
+        image = []
+        try:
+            img = self.config.get('IMAGE','image')
+            data = self.config.get('IMAGE','data')
+        except Exception as error:
+            logger = MyLogger()
+            logger.error('图片配置信息读取失败：%s' % error)
+            exit()
+        image.append(int(img))
+        image.append(int(data))
+        return image
+    
+    def get_videoslice(self):
+        """
+        返回视频片段配置信息的列表[视频片段个数,]
+        """
+        videoslice = []
+        try:
+            video = self.config.get('VIDEOSLICE','videoslice')
+        except Exception as error:
+            logger = MyLogger()
+            logger.error('视频片段配置信息读取失败：%s' % error)
+            exit()
+        videoslice.append(int(video))
+        return videoslice
+    
+    def get_person(self):
+        pass
 
-    def get_person_num(self):
-        return self.person_num
+    def get_face(self):
+        pass
 
-    def get_face_num(self):
-        return self.face_num
+    def get_motor(self):
+        pass
 
-    def get_motorvehicle_num(self):
-        return self.motorvehicle_num
-
-    def get_data_type(self):
-        return self.data_type
-
-    def get_report_path(self):
-        return self.report_file
+    def get_report(self):
+        """
+        返回测试报告信息的列表[测试报告路径,测试报告名称]
+        """
+        try:
+            report = self.config.get('REPORT', 'report')
+        except Exception as error:
+            logger = MyLogger()
+            logger.error('测试报告信息读取失败：%s' % error)
+        return report
 
     def get_host(self):
-        return self.host
-
-    def get_sub_num(self):
-        return self.sub_num
+        """
+        返回服务器地址
+        """
+        try:
+            host = self.config.get('REPORT', 'report')
+        except Exception as error:
+            logger = MyLogger()
+            logger.error('服务器地址信息读取失败：%s' % error)
+        return host
